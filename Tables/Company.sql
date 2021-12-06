0675977bc985d02a47e70ea5dcd834ab0df02268
Angel Pineda <pinedap.angel@gmail.com>
2021-12-06 13:54:09 -0500

    Modify(Company): Change Name, Address and ContactName from VARCHAR -> NVARCHAR.

--- Tables/Company.sql
+++ Tables/Company.sql
@@ -3,10 +3,10 @@ USE AEC;
 CREATE TABLE Company
 (
 	CompanyId INT IDENTITY(10001, 1) PRIMARY KEY,
-	[Name] VARCHAR(15) NOT NULL,
-	[Address] VARCHAR(100) NOT NULL,
+	[Name] NVARCHAR(15) NOT NULL,
+	[Address] NVARCHAR(100) NOT NULL,
 	PhoneNumber VARCHAR(15) NOT NULL,
-	ContactName VARCHAR(45) NOT NULL,
+	ContactName NVARCHAR(45) NOT NULL,
 
 	CONSTRAINT CHECK_Company_PhoneNumber CHECK (PhoneNumber LIKE '+% %-%-%')
 );
