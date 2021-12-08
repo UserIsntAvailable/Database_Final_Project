b794135d8604469acbcaf55148df135e6d0ae83a
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-12-08 01:34:18 -0500

    Fix(JobPlacement): Add missing PK

--- Tables/JobPlacement.sql
+++ Tables/JobPlacement.sql
@@ -5,6 +5,8 @@ CREATE TABLE JobPlacement
 	OpeningId INT FOREIGN KEY REFERENCES JobOpening(OpeningId),
 	CandidateId INT FOREIGN KEY REFERENCES Candidate(CandidateId),
 	TotalHours INT NOT NULL
+
+	CONSTRAINT PK_OPEN_CAND PRIMARY KEY(OpeningId, CandidateId)
 );
 
 GO;
