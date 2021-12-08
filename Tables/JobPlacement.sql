4863042a5ade3f3bd1e87fccaec1f8f71ae5e8a8
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-12-08 01:36:07 -0500

    Fix(JobPlacement): Add DEFAULT 0, CHECK > 0 constraints to TotalHours

--- Tables/JobPlacement.sql
+++ Tables/JobPlacement.sql
@@ -4,9 +4,10 @@ CREATE TABLE JobPlacement
 (
 	OpeningId INT FOREIGN KEY REFERENCES JobOpening(OpeningId),
 	CandidateId INT FOREIGN KEY REFERENCES Candidate(CandidateId),
-	TotalHours INT NOT NULL
+	TotalHours INT DEFAULT 0 NOT NULL,
 
-	CONSTRAINT PK_OPEN_CAND PRIMARY KEY(OpeningId, CandidateId)
+	CONSTRAINT PK_OPEN_CAND PRIMARY KEY(OpeningId, CandidateId),
+	CONSTRAINT Check_JobPlacement_TotalHours CHECK(TotalHours >= 0)
 );
 
 GO;
